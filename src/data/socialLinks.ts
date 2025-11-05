3c3251048c58f4a92aa5e5e6220d88a854e1dbf0
furqansoftengr <furqanjee.softengr@gmail.com>
2025-11-05 05:02:03 +0500

    Resolve merge changes 2

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,24 +1,30 @@
 import { SocialLink } from '../types';
 
+// Import icons locally
+import DribbbleIcon from '../icons/dribbble.svg';
+import BehanceIcon from '../icons/behance.svg';
+import InstagramIcon from '../icons/instagram.svg';
+import MailIcon from '../icons/mail.svg';
+
 export const socialLinks: SocialLink[] = [
-  { 
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dribbble/dribbble-original.svg', 
-    href: 'https://dribbble.com/elaravance', 
-    label: 'Dribbble' 
-  },
-  { 
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/behance/behance-original.svg', 
-    href: 'https://behance.net/elaravance', 
-    label: 'Behance' 
-  },
-  { 
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/instagram/instagram-original.svg', 
-    href: 'https://instagram.com/elaravance', 
-    label: 'Instagram' 
-  },
-  { 
-    icon: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg', 
-    href: 'mailto:hello@elaravance.com', 
-    label: 'Gmail' 
-  },
+    {
+        icon: DribbbleIcon,
+        href: 'https://dribbble.com',
+        label: 'Dribbble'
+    },
+    {
+        icon: BehanceIcon,
+        href: 'https://behance.net',
+        label: 'Behance'
+    },
+    {
+        icon: InstagramIcon,
+        href: 'https://instagram.com',
+        label: 'Instagram'
+    },
+    {
+        icon: MailIcon,
+        href: 'mailto:hello@elaravance.com',
+        label: 'Mail'
+    },
 ];
